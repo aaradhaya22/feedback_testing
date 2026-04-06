c9e20532c6958b887219f593c0b6dfdb00628522
himanshuyadav71 <hyadavmp41@gmail.com>
2026-04-06 13:53:52 +0530

    Deploy Configs: Add dj-database-url, whitenoise middleware, build script, and set CORS

--- Backend/feedbacksystem/settings.py
+++ Backend/feedbacksystem/settings.py
@@ -15,6 +15,7 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,10 @@ ALLOWED_HOSTS = [
     '*', # Final fallback for flexibility
 ]
 
+# Static files for deployment
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Application definition
@@ -62,6 +67,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'feedback_app.middleware.CsrfExemptMiddleware',  # Custom CSRF exempt middleware
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,7 +78,7 @@ MIDDLEWARE = [
 
 
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://clg-project-sand.vercel.app",
     "https://teachers-feedback-app-sand.vercel.app",
@@ -167,14 +173,11 @@ WSGI_APPLICATION = 'feedbacksystem.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "postgres"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),   
-    }
+    "default": dj_database_url.config(
+        default=f"postgres://{os.getenv('DB_USER', 'postgres')}:{os.getenv('DB_PASSWORD', 'postgres')}@{os.getenv('DB_HOST', 'localhost')}:{os.getenv('DB_PORT', '5432')}/{os.getenv('DB_NAME', 'postgres')}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
